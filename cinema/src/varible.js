7e0660fa63b3c09143398a10f710f676d31be43f
Phanuwat2828 <131578554+Phanuwat2828@users.noreply.github.com>
2024-12-03 12:09:21 +0700

    update

---
+++ cinema/src/varible.js
@@ -0,0 +1,42 @@
+const movie = {
+    "movie1":{
+        "name":"Arcane",
+        "midb":9.5,
+        "image":"https://i.pinimg.com/736x/1e/3c/fb/1e3cfbc50ed3306e22c0802a866826da.jpg",
+        "image_bg":"https://m.media-amazon.com/images/M/MV5BMWY0MWE3MmUtMmQ0ZS00Yzc2LTk5ZDEtMzYwYmE3ODA4ZWI5XkEyXkFqcGc@._V1_FMjpg_UX1800_.jpg",
+        "detail":"You don’t need to be an anime fan or a gamer to appreciate the magic of Netflix's mega-hit “Arcane.” Based on the online multiplayer game “League of Legends,” the show is surprisingly accessible, with a gorgeous mix of 2D and 3D animation, compelling characters, and a rich narrative that tackles big ideas while staying grounded in human relationships. The first season won over critics and fans alike, scoring a 9.0 rating on IMDb and quietly winning a Primetime Emmy for Outstanding Animated Program. Season 2 finds conflict simmering between the “haves” of utopian city Piltover and the “have-nots” of the underground Zaun, but also between estranged sisters Jinx (Ella Purnell) and Vi (Hailee Steinfeld). Although the series concludes in this final season, Linke has teased there are more “League” stories to tell. ",
+        "vidoe":"https://www.youtube.com/embed/Pj-2KzHjjD0?si=9MmmoftHleZqKo4J"
+    },
+    "movie2":{
+        "name":"Moana 2",
+        "midb":7.1,
+        "image":"https://i.pinimg.com/736x/43/0f/14/430f140dbde664145e19e09d74105ce5.jpg",
+        "image_bg":"https://m.media-amazon.com/images/M/MV5BYTczYjYwMjktMGQ2My00NGRiLTlhY2EtMzE0ZTI0OTIxYjJlXkEyXkFqcGc@._V1_FMjpg_UX1800_.jpg",
+        "detail":"Moana reunites with Maui after a mysterious call from her ancestors sets her off on a new high seas adventure. The sequel was inevitable, especially considering Moana was the most streamed movie of 2023 — not bad for a movie that was released in 2016. Auli’i Cravalho heads up the returning voice cast alongside Dwayne Johnson, with Temuera Morrison as Chief Tui, Rachel House as Gramma Tala, and Alan Tudyk as Heihei the rooster. And did you know the live-action movie is filming currently?",
+        "vidoe":"https://www.youtube.com/embed/hDZ7y8RP5HE?si=6UxV0-mbSVxQZbp7"
+    },
+    "movie3":{
+        "name":"Wicked",
+        "midb":8.1,
+        "image":"https://i.pinimg.com/736x/36/10/84/36108435e6845f06e0103360cff7079f.jpg",
+        "image_bg":"https://m.media-amazon.com/images/M/MV5BNTc5NmI2ODktMDMwOS00NzIxLWIwNGUtNmUwMTRiNWZjMTI3XkEyXkFqcGc@._V1_FMjpg_UX1800_.jpg",
+        "vidoe":"https://www.youtube.com/embed/GpGByJa1Gb8?si=PLQhaXj8zRluSYMk",
+        "detail":"If you’re new to the world of Wicked, here’s what you need to know. In 1995, author Gregory Maguire wrote the novel “Wicked: The Life and Times of the Wicked Witch of the West”, a revisionist origin story about Elphaba, the antagonist from Frank L. Buam’s “The Wizard of Oz.” Then in 2003, the popular book was adapted into a Broadway musical that focused on the complicated relationship between Elphaba and her school roommate Glinda. Now, director John M. Chu is bringing this beloved fantasy tale to the silver screen with a musical movie starring Cynthia Erivo and Ariana Grande as the bewitching duo whose passion, talents, and personalities bring them together before fate takes them down very different paths."
+    },"movie4":{
+        "name":"Squid Game",
+        "midb":8,
+        "image":"https://i.pinimg.com/736x/26/73/02/2673020dcb758c851483e769f96c3154.jpg",
+        "image_bg":"https://www.cnet.com/a/img/resize/5bccae86b3ab1fa5a1be6a277209818b9c0b1d31/hub/2024/09/18/60900259-ccd5-484f-b77c-3c6a6af21808/en-us-squids2-teaser-thedrag-vertical-27x40-srgb-pre.jpg?auto=webp&fit=crop&height=675&width=1200",
+        "vidoe":"https://www.youtube.com/embed/JqR4mVk4JgQ?si=E0EHD1vdhTl62zUw",
+        "detail":"It’s been three years since the entire world cheered on Gi-hun (Lee Jung-jae) as he escaped a lethal game show and vowed to track down those responsible. Now, the unlikely hero returns to make good on his promise in Season 2, which sees Player 456 head back into the game with a whole new group of contestants ready to set their humanity aside for a chance at the grand prize. Prepare yourself for more tense conflicts, shocking reveals, and the return of Young-hee the murderous “Red Light, Green Light” doll. The new season drops on December 26, and we can’t think of a better way to close out the holidays than with this grim exploration of greed, capitalism, and human perseverance."
+    }
+    
+}
+
+// "movie3":{
+//         "name":"",
+//         "midb":0,
+//         "image":"",
+//         "image_bg":"",
+//         "vidoe":""
+//     },
